7ee341b17f6466d6fd06ceac7d7dc4daad3d77cb
Ncomgenius <94155166+ncomgenius@users.noreply.github.com>
2022-09-21 02:03:26 +0700

    supdate

--- s_work/assets/js/DG---BS5-Countdown.js
+++ s_work/assets/js/DG---BS5-Countdown.js
@@ -8,7 +8,7 @@
       mm = String(today.getMonth() + 1).padStart(2, "0"),
       yyyy = today.getFullYear(),
       nextYear = yyyy + 1,
-      dayMonth = "09/16/",
+      dayMonth = "09/16/ 12:00:00",
       birthday = dayMonth + yyyy;
   
   today = mm + "/" + dd + "/" + yyyy;
